c5c16b8bba1a954285a0a3a527779ffab835fa25
SupharakRungram <142819660+SupharakRungram@users.noreply.github.com>
2023-08-21 23:02:41 +0700

    first commit

---
+++ app/Http/Controllers/ContentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ContentRequest;
+use App\Models\Content;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Http\Request;
+
+class ContentController extends Controller
+{
+    public function index()
+    {
+        $contents = Content::paginate(5);
+        return view('content.index', compact('contents'));
+    }
+
+    public function create()
+    {
+        $contents = new Content;
+        return view('content.form', compact('contents'));
+    }
+
+    // public function logout()
+    // {
+    //     // if(Auth::attempt($credentials)){
+    //     //     $request->session()->destroy();
+    //     //     return redirect()->intended('/login');
+    //     // }
+    //     Auth::logout();
+    //     return redirect('/login');
+    // }
+
+    public function store(ContentRequest $request)
+    {
+        $content = new Content;
+
+        $this->save($content, $request);
+        return redirect('/content');
+    }
+
+    public function edit($id)
+    {
+        $contents = Content::findOrFail($id);
+        return view('content.form', compact('contents'));
+    }
+
+    private function save($data, $value)
+    {
+        $data->topic = $value->topic;
+        $data->description = $value->description;
+        $data->tags = $value->tags;
+        $data->links = $value->links;
+        //$data->status = true;
+        $data->user_id = 1;
+        $data->save();
+    }
+
+    public function update(ContentRequest $request, $id)
+    {
+        $content = Content::findOrFail($id);
+
+        $this->save($content, $request);
+        return redirect('/content');
+    }
+
+    public function destroy($id)
+    {
+        /*Content::destroy($id);
+        $this->index();*/
+        $content = Content::findOrFail($id);
+        if ($content->status) {
+            $content->status = false;
+        } else {
+            $content->status = true;
+        }
+        $content->save();
+
+    }
+
+}
